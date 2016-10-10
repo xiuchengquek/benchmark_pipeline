62a203c359d9224df50c7d08eaf85539c12ecbed
xiuchengquek <x.quek@garvan.org.au>
2016-10-10 17:35:25 +1100

    fixed stashes

--- src/run_carna.py
+++ src/run_carna.py
@@ -12,7 +12,7 @@ with open(sys.argv[1]) as f:
     for line in f:
         fields = line.strip().split('\t')
 
-        print('locarna --noLP  --write_structures  --clustal=out/locarna/clustalw/{alignment} data/fasta/{sequence_a}.fa data/fasta/{sequence_b}.fa'.format(
+        print('locarna --noLP  --write-structure  --clustal=out/locarna/clustalw/{sequence_a}_vs_{sequence_b}.clustalw data/fasta/{sequence_a}.fa data/fasta/{sequence_b}.fa'.format(
             alignment = fields[0],
             sequence_a =fields[1],
             sequence_b = fields[2]
