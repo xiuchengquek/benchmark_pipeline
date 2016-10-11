404d89ebc1977ea807860b5d2a556a18c9387c9e
xiuchengquek <x.quek@garvan.org.au>
2016-10-12 03:35:35 +1100

    added changes

--- src/run_rnalifold_stockhelm_carna.py
+++ src/run_rnalifold_stockhelm_carna.py
@@ -20,15 +20,15 @@ clustal_files = os.listdir(clustal_folder)
 
 for x in clustal_files:
     rnaalifold_files = os.path.join(structure_folder, x)
-    rnaalifold_files.replace('.clustalw', '.rnafold.tsv')
+    rnaalifold_files = rnaalifold_files.replace('.clustalw', '.rnafold.tsv')
 
     clustal_path = os.path.join(clustal_folder, x)
 
     out_path = os.path.join(output_folder, x)
-
+    out_path = out_path.replace('.clustalw', '.stock')
 
     if os.path.exists(rnaalifold_files):
-        print('{script_file} {rnaali} {clustal} {out_path}')
+        print('python {script_file} {rnaali} {clustal} {out_path}'.format(script_file = script_file, rnaali = rnaalifold_files, clustal = clustal_path, out_path = out_path))
     else:
         raise Exception('missing file %s' % rnaalifold_files)
 
