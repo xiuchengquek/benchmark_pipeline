850e79993d15e189d599c18f29203926b989c59a
xiuchengquek <biobirdman@gmail.com>
2016-10-13 18:33:05 +1100

    added comparision code

---
+++ src/run_compare_ct.py
@@ -0,0 +1,43 @@
+
+
+import sys
+import os
+
+reference = sys.argv[1]
+tested = sys.argv[2]
+out = sys.argv[3]
+error = sys.argv[4]
+
+script_file = '/home/xiuque/Projects/DotAlignerMatthew/paul/compare_ct.pl'
+
+
+
+reference_files = os.listdir(reference)
+
+
+fh_err = open(error, 'w')
+
+
+for x in reference_files:
+    test_file = os.path.join(tested, x)
+    ref_file = os.path.join(reference, x)
+
+
+
+    out_path = os.path.join(out, x)
+    out_path = out_path.replace('.ct', '.results')
+
+    if os.path.exists(test_file):
+        print('{script_file} {ref} {test} > {out_path}'.format(script_file = script_file, ref = ref_file, test = test_file, out_path = out_path))
+    else:
+         fh_err.write('missing file %s\n' % test_file)
+
+
+
+
+fh_err.close()
+
+
+
+
+
