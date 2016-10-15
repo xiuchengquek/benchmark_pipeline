b4a83b729865f3ecb757dd59d145fe6c949a33e3
xiuchengquek <biobirdman@gmail.com>
2016-10-15 20:12:08 +1100

    added tagrument

--- src/sci_diff.py
+++ src/sci_diff.py
@@ -69,7 +69,7 @@ def parse_clustalw(stream) :
 
     for line in stream:
         line = line.strip()
-        if line:
+        if line and not '(' not in line:
             sequenceA = line
             sequenceA_id, sequenceA_fasta  = sequenceA.split()
             sequenceB = next(stream).strip()
